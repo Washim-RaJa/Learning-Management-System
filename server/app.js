ffd0f008035566236dd03d56634469ff0f9216d7
Washim-RaJa <washimrajam75@gmail.com>
2024-09-27 23:17:31 +0530

    initialized server side setup

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { config } = require('dotenv');
+config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+app.use('/ping', (req, res)=> {
+    res.send('Pong')
+})
+
+// routes of 3 modules
+app.all('*', (req, res)=>{
+    res.status(404).send("Oops!! 404 page not found!")
+})
+
+
+module.exports = app;
