b94d9eda356c21b5daa60252f5144e5d28c3067e
Washim-RaJa <washimrajam75@gmail.com>
2024-10-10 22:32:06 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ import { config } from "dotenv";
 import connectionToDB from "./config/dbConnection.js";
 import cloudinary from 'cloudinary';
 import Razorpay from 'razorpay';
-import path from 'path';
-import express from "express";
 
 config();
 
@@ -22,12 +20,6 @@ export const razorpay = new Razorpay({
     key_secret: process.env.RAZORPAY_SECRET
 });
 
-const __dirname = path.resolve()
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
-})
 
 app.listen(PORT, async () => {
     await connectionToDB();
