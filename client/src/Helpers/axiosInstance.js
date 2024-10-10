9a430490b779692b5b473ffd3a03d7748b7146cd
Washim-RaJa <144546487+Washim-RaJa@users.noreply.github.com>
2024-10-10 22:44:44 +0530

    added backend URL from render  axiosInstance.js

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 const axiosInstance = axios.create();
 
-axiosInstance.defaults.baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/`;
+// axiosInstance.defaults.baseURL = `${import.meta.env.VITE_API_BASE_URL}/api/v1/`;
+
+axiosInstance.defaults.baseURL = `https://skillify-server.onrender.com/api/v1/`;
 
 axiosInstance.defaults.withCredentials = true;
 
